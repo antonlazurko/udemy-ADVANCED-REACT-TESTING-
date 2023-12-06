c92b268e2be1dc5bf56c15bf2ef5c58ba5be9848
Anton <antla@ciklum.com>
2023-12-06 18:11:16 +0200

    add each route test

--- base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
@@ -1,10 +1,31 @@
 import { App } from "../../../App";
 import { render, screen } from "../../../test-utils";
 
-test("home page does not redirect to login screen", () => {
-  render(<App />);
-  const heading = screen.getByRole("heading", {
-    name: /welcome/i,
-  });
-  expect(heading).toBeInTheDocument();
-});
+test.each([
+  {
+    routeName: "Home",
+    routePath: "/",
+    headingMatch: /welcome/i,
+  },
+  {
+    routeName: "Band 1",
+    routePath: "/bands/1",
+    headingMatch: /joyous/i,
+  },
+  {
+    routeName: "Shows",
+    routePath: "/shows",
+    headingMatch: /upcoming/i,
+  },
+])(
+  "$routeName home page does not redirect to login screen",
+  async ({ routePath, headingMatch }) => {
+    render(<App />, {
+      routeHistory: [routePath],
+    });
+    const heading = await screen.findByRole("heading", {
+      name: headingMatch,
+    });
+    expect(heading).toBeInTheDocument();
+  }
+);
