243d17e016480180c31e2817eb418a35c71c6f88
Anton <antla@ciklum.com>
2023-12-07 11:45:54 +0200

    add sign in/up each tests

--- base-app/client/src/features/auth/tests/protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -32,23 +32,73 @@ test.each([
   expect(signInHeader).toBeInTheDocument();
 });
 
-test("succesful sign-in flow", async () => {
+// test("succesful sign-in flow", async () => {
+//   const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
+
+//   const emailField = screen.getByLabelText(/email/i);
+//   userEvent.type(emailField, "booking@avancheogcheese.com");
+
+//   const passwordlField = screen.getByLabelText(/password/i);
+//   userEvent.type(passwordlField, "iheartcheese");
+
+//   const signInForm = screen.getByTestId("sign-in-form");
+
+//   const signInBtn = getByRole(signInForm, "button", {
+//     name: /sign in/i,
+//   });
+//   userEvent.click(signInBtn);
+//   await waitFor(() => {
+//     expect(history.location.pathname).toBe("/tickets/1");
+//   });
+//   expect(history.entries).toHaveLength(1);
+// });
+// test("succesful sign-up flow", async () => {
+//   const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
+
+//   const emailField = screen.getByLabelText(/email/i);
+//   userEvent.type(emailField, "booking@avancheofcheese.com");
+
+//   const passwordlField = screen.getByLabelText(/password/i);
+//   userEvent.type(passwordlField, "iheartcheese");
+
+//   const signUpBtn = screen.getByRole("button", {
+//     name: /Sign up/i,
+//   });
+//   userEvent.click(signUpBtn);
+//   await waitFor(() => {
+//     expect(history.location.pathname).toBe("/tickets/1");
+//   });
+
+//   expect(history.entries).toHaveLength(1);
+// });
+
+test.each([
+  {
+    testName: "sign-in",
+    buttonName: /sign in/i,
+  },
+  {
+    testName: "sign-in",
+    buttonName: /sign in/i,
+  },
+])("succesful $testName flow", async ({ buttonName }) => {
   const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
 
   const emailField = screen.getByLabelText(/email/i);
-  userEvent.type(emailField, "booking@avancheogcheese.com");
+  userEvent.type(emailField, "booking@avancheofcheese.com");
 
   const passwordlField = screen.getByLabelText(/password/i);
   userEvent.type(passwordlField, "iheartcheese");
 
   const signInForm = screen.getByTestId("sign-in-form");
 
-  const signInBtn = getByRole(signInForm, "button", {
-    name: /sign in/i,
+  const testButtonName = getByRole(signInForm, "button", {
+    name: buttonName,
   });
-  userEvent.click(signInBtn);
+  userEvent.click(testButtonName);
   await waitFor(() => {
     expect(history.location.pathname).toBe("/tickets/1");
   });
+
   expect(history.entries).toHaveLength(1);
 });
